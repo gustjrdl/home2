6bfac29890cdd39e3418f939dcd8edc8ed4e13b2
[gustjrdl] <[shsopen@gmail.com]>
2024-02-07 00:43:36 +0900

    240207

---
+++ src/Question/Main.java
@@ -0,0 +1,20 @@
+package Question;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public void solution() throws Exception {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter wt = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a, b, c = Integer.parseInt(rd.readLine());
+    }
+    public static void main(String args[]) throws Exception {
+
+        new Main().solution();
+    }
+}
