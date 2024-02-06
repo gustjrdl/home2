3df7123d65a911a17716c3594addd98f214e3697
[gustjrdl] <[shsopen@gmail.com]>
2024-02-07 01:27:42 +0900

    20240207

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 import java.io.*;
 import java.util.Scanner;
-import java.util.StringTokenizer;
+
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) throws Exception{
         Scanner sc = new Scanner(System.in);
 
+        long a= sc.nextLong();
+        long b= sc.nextLong();
+        long c= sc.nextLong();
 
+        long ac = (long) Math.pow(10, 12);
 
+        if (1 <= a && a<= ac &&1 <= b && b<= ac&& 1 <= c && c<= ac)
+        {
+            System.out.println(a+b+c);
+        }
     }
 }
--- src/Question/Main.java
+++ src/Question/Main.java
@@ -4,14 +4,23 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class Main {
 
     public void solution() throws Exception {
-        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter wt = new BufferedWriter(new OutputStreamWriter(System.out));
+        Scanner sc = new Scanner(System.in);
 
-        int a, b, c = Integer.parseInt(rd.readLine());
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+
+        int ac = (int) Math.pow(10, 12);
+
+        if (1 <= a && a<= ac &&1 <= b && b<= ac&&1 <= c && c<= ac)
+        {
+            System.out.println(a+b+c);
+        }
     }
     public static void main(String args[]) throws Exception {
 
