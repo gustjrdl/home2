f2068dc3871f258bab24a5ddb00c4d98d2120ac2
[gustjrdl] <[shsopen@gmail.com]>
2024-02-21 00:14:18 +0900

    20240221

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,31 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String args[]) throws Exception{
+    public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        int h = sc.nextInt();
-        int m = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         int c = sc.nextInt();
 
-        h += c / 60;
-        m += c % 60;
-
-        if(m >= 60) {
-            h+=1;
-            m-=60;
+        if(a == b && b == c){
+            System.out.println(10000+a*1000);
+        } else if (a==b || b==c) {
+            System.out.println(1000+b*100);
         }
-        if(h >= 24) {
-            h-=24;
+        else if(a == c){
+            System.out.println(1000+a*100);
         }
-        System.out.println(h + " " + m);
-        System.out.println();
-
+        else {
+            if(a>=b&&a>=c)
+            {
+                System.out.println(a*100);
+            }
+            else if (b>=a&&b>=c) {
+                System.out.println(b*100);
+            }else if (c>=b&&c>=a) {
+                System.out.println(c*100);
+            }
+        }
     }
-
 }
-
