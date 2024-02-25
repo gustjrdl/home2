56541995f70ea4fde3986bbba0eaae71acbe02ec
[gustjrdl] <[shsopen@gmail.com]>
2024-02-26 00:22:39 +0900

    20240226(2)

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
 
+        Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        int b = sc.nextInt();
+
         int sum =0;
-        for(int i = 1; a >= i; i++) {
-            sum = i+sum;
+
+        for (int i = 1; b >= i; i++) {
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+
+            sum += c*d;
         }
-        System.out.println(sum);
+        if(sum != a){
+            System.out.println("No");
+        }else {
+            System.out.println("Yes");
+        }
+
+
+
     }
 }
