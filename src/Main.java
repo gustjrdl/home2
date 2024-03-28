a97ce8232f9a8e01d81ca9014a04706ff5340a28
[gustjrdl] <[shsopen@gmail.com]>
2024-03-29 01:01:29 +0900

    20240329

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -7,15 +8,18 @@ public class Main {
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
 
         int a = Integer.parseInt(br.readLine());
+        StringTokenizer st;
 
-        for(int i =1; a >= i; i++){
+        for (int i = 1; i <= a; i++) {
             st = new StringTokenizer(br.readLine());
+            int b = (Integer.parseInt(st.nextToken()));
+            int c = (Integer.parseInt(st.nextToken()));
 
-            bw.write("Case #"+i+": "+(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()))+"\n");
+            bw.write("Case #"+i+": "+b+" + "+c+" = "+(b+c)+"\n");
         }
+
         bw.flush();
         bw.close();
 
